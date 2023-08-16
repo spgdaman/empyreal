26da1c6540d6747119e30b8460168edb54c53a2b
SPG <gatheru.simon432@gmail.com>
2023-08-16 19:42:07 +0300

    api views added

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
-# Create your views here.
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
