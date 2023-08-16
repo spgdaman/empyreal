2224083668a1fcfb66c58af6e0f5e7da289b6566
SPG <gatheru.simon432@gmail.com>
2023-08-16 19:25:49 +0300

    api app created, drf installed and app registered in settings

--- accounts/models.py
+++ accounts/models.py
@@ -88,15 +88,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.email}"
     
-    # def has_perm(self, perm, obj=None):
-    #     "Does the user have a specific permission?"
-    #     # Simplest possible answer: Yes, always
-    #     return True
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
     
-    # def has_module_perms(self, app_label):
-    #     "Does the user have permissions to view the app 'app_label'?"
-    #     # Simplest possible answer: Yes, always
-    #     return True
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app 'app_label'?"
+        # Simplest possible answer: Yes, always
+        return True
     
     # @property
     # def is_staff(self):
@@ -106,7 +106,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # @property
     # def is_admin(self):
     #     "Is the user a admin member?"
-    #     return self.admin
+    #     return self.is_admin
     
     objects = UserManager()
 
--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts',
+    'api',
     
     'bootstrap4',
     'crispy_forms',
