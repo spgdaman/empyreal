597c46d3f313e71635edc2c22c55f96963f6ef42
SPG <gatheru.simon432@gmail.com>
2023-08-16 19:27:51 +0300

    api urls registered

--- core/urls.py
+++ core/urls.py
@@ -22,10 +22,10 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
-
+    
+    path('api/', include('api.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    # path('accounts/', include('accounts.urls')),
 ]
 
 # from django.conf import settings
