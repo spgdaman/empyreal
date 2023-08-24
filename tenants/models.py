bd7b3a5b25f7fb7c2a7737aebe649ecefd97c9d3
SPG <gatheru.simon432@gmail.com>
2023-08-24 22:25:54 +0300

    tenant model created

--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django_multitenant.fields import *
+from django_multitenant.models import *
 # Create your models here.
+
+class Tenant(TenantModel):
+    name = models.CharField(max_length=50)
+    admin_first_name = models.CharField(max_length=50)
+    admin_last_name = models.CharField(max_length=50)
+    admin_email = models.CharField(max_length=50)
+    admin_phone_number = models.IntegerField(null=False, blank=False)
+    class TenantMeta:
+        tenant_field_name = "id"
